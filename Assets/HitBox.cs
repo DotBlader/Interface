8ee9bfd832962e0aaa93f32bcdd25e78310ff19a
Fredrik Ehrs <freddicatthecow@gmail.com>
2019-04-12 13:31:46 +0200

    12/04

---
+++ Assets/HitBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    public bool isTouching;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isTouching = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isTouching = false;
+    }
+}
